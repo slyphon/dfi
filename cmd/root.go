6e1f3621bf8522ab0b0162d0fb101bd1148129a7
Jonathan Simms <slyphon@gmail.com>
2020-02-05 18:14:20 -0500

    change name

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+Copyright © 2020 Jonathan Simms <slyphon@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -31,14 +31,71 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-  Use:   "dfi-go",
-  Short: "A brief description of your application",
-  Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+  Use:   "dfi",
+  Short: "Manages dotfile symlinks to version-controlled files",
+  Long: `The purpose of this utility is to keep configuration files and directories
+  under source control in a single directory, then symlink them into place.
+  This program will by default look in the current working directory for a
+  directory named 'dotfiles', and will create symlinks to all of them in the
+  parent of the current directory with a '.' prepended.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+  $HOME/
+    .settings/
+      dotfiles/
+        bashrc
+        bash_profile
+        zshrc
+        ssh/
+          config
+
+  if you cd into '~/.settings' and run dotinstall it will create the following symlinks:
+
+  $HOME/
+    .bashrc -> .settings/dotfiles/bashrc
+    .bash_profile -> .settings/dotfiles/bash_profile
+    .zshrc -> .settings/dotfiles/zshrc
+    .ssh -> .settings/dotfiles/ssh
+
+  Additionally, it can install links under a 'bin' directory, where the '.' prefix
+  is not applied. This is useful when you have a number of shell scripts and want
+  to link them from your version controllled directory into a location in your PATH.
+
+  $HOME/
+    .settings/
+      bin/
+        foo
+        bar
+        baz
+
+  can be linked to
+
+  $HOME/
+    .local/
+      bin/
+        foo -> ../../.settings/bin/foo
+        bar -> ../../.settings/bin/bar
+        baz -> ../../.settings/bin/baz
+
+  In the case of conflicts (i.e. destination already exists) you can decide
+  how files and symlinks will be handled.
+
+  If a link path already exists and is a file, the following strategies are available:
+
+  * 'backup': move the file to a unique dated backup location and create the symlink
+
+  * 'replace': just delete the file and create the symlink
+
+  * 'warn': print a warning that the conflict exists and continue.
+
+  * 'fail': stop processing and report an error.
+
+  If a symlink exists:
+
+  * 'replace': assume we own the symlink and recreate it pointing to the target
+
+  * 'warn': print a warning that the conflict exists and continue
+
+  * 'fail': stop processing and report an error`,
   // Uncomment the following line if your bare application
   // has an action associated with it:
   //	Run: func(cmd *cobra.Command, args []string) { },
@@ -84,7 +141,7 @@ func initConfig() {
 
     // Search config in home directory with name ".dfi-go" (without extension).
     viper.AddConfigPath(home)
-    viper.SetConfigName(".dfi-go")
+    viper.SetConfigName(".dfi")
   }
 
   viper.AutomaticEnv() // read in environment variables that match
