273a944d00827e0960a282ea870715ededae6208
Jonathan Simms <slyphon@gmail.com>
2020-02-08 02:07:33 -0500

    forgot to add the testhelper

---
+++ cmd/root_test.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	df "github.com/slyphon/dfi/internal/dotfile"
+	testhelp "github.com/slyphon/dfi/internal/testhelper"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/suite"
+	log "github.com/sirupsen/logrus"
+)
+
+type (
+	RootCmdSuite struct {
+		testhelp.RequireSuite
+		tmpdir string
+	}
+
+	RunMock struct {
+		settings *df.Settings
+	}
+)
+
+func (rm *RunMock) Run(s *df.Settings) error {
+	rm.settings = s
+	return nil
+}
+
+func TestRootCmdSuite(t *testing.T) {
+	s := new(RootCmdSuite)
+	s.AddBeforeHook(func (a, b string) {
+		var err error
+		s.tmpdir, err = ioutil.TempDir("", "rootcmdsuite")
+		s.NoError(err)
+	})
+	s.AddAfterHook(func (a, b string){
+		if (s.tmpdir != "") {
+			err := os.RemoveAll(s.tmpdir)
+			log.Errorf("ignoring error tearing down tmpdir: %+v", err)
+		}
+	})
+	suite.Run(t, s)
+}
+
+func (s *RootCmdSuite) TestCommandSanity() {
+	rm := &RunMock{}
+	var rootCmd *cobra.Command
+
+	rootCmd = NewRootCommand(rm.Run)
+	rootCmd.SetArgs([]string{"-p", ".", "/a/b/c/settings", "/a/b/c/home"})
+	err := rootCmd.Execute()
+	s.NoError(err)
+	s.Equal(".", rm.settings.Prefix)
+	s.Equal([]string{"/a/b/c/settings"}, rm.settings.SourcePaths)
+	s.Equal("/a/b/c/home", rm.settings.DestPath)
+}
